a7e763b8a2321112529559ace226a6162f6a0468
MartinsRiss <38617937+MartinsRiss@users.noreply.github.com>
2019-05-14 18:06:27 +0300

    upload/download
    cerība un sapņi(cerams)

--- MajasLapa/ligumi.php
+++ MajasLapa/ligumi.php
@@ -1,12 +1,21 @@
 <?php
-$hostname = "localhost";
-$username = "user";
-$password = "";
-$databaseName="writers";
-
-$conn = mysqli_connect("localhost", "root", "");
-
-mysqli_select_db($conn, "writers");
+$conn=new PDO('mysql:host=localhost; dbname=demo', 'root', '') or die(mysql_error());
+if(isset($_POST['submit'])!=""){
+    $name=$_FILES['photo']['name'];
+    $size=$_FILES['photo']['size'];
+    $type=$_FILES['photo']['type'];
+    $temp=$_FILES['photo']['tmp_name'];
+    $caption1=$_POST['caption'];
+    $link=$_POST['link'];
+    move_uploaded_file($temp,"files/".$name);
+    $query=$conn->query("insert into upload(name)values('$name')");
+    if($query){
+        header("location:ligumi.php");
+    }
+    else{
+        die(mysql_error());
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,10 +23,16 @@ mysqli_select_db($conn, "writers");
 
 <head>
 <title>iDala līgumi</title>
-<link rel="icon" href="favicon.ico" type="image/ico">
-<link rel="stylesheet" type="text/css" href="style.css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/DT_bootstrap.css">
+    <link rel="icon" href="favicon.ico" type="image/ico">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
 
+    <script type="text/javascript" charset="utf-8" language="javascript" src="js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf-8" language="javascript" src="js/DT_bootstrap.js"></script>
 <body>
 
 <div class="header">
@@ -40,21 +55,47 @@ mysqli_select_db($conn, "writers");
     <div class="cardDifferent">
       <h2 align="center">iDala ievietotie līgumi</h2>
 	</div>
-<?php
-$sql = "SELECT id, name, email, title, content FROM writing ORDER BY id DESC;";
-$result = $conn->query($sql);
+      <div class="cardDifferent">
+      <div class="row-fluid">
+          <div class="span12">
+              <div class="container">
 
-if ($result->num_rows > 0) {
-// output data of each row
-	while($row = $result->fetch_assoc()) {
-        echo " <div class='card'><br> <b>Autors:</b> " . $row["name"].  " <br> <b>Epasta adrese:</b> " . $row["email"].  "<br><b>Virsraksts:</b> " . $row["title"].  "<br> <b>Darbs:</b> " . $row["content"]. "<br><br> </div>";
-	} 
-}
-else {
-	echo "<div class='card'> Nav ievietots neviens darbs! </div>";
-}
-?>  
- 
+                  <h1><p>Upload  And  Download Files</p></h1>
+                  <br />
+                  <br />
+                  <form enctype="multipart/form-data" action="" name="form" method="post">
+                      Select File
+                      <input type="file" name="photo" id="photo" /></td>
+                      <input type="submit" name="submit" id="submit" value="Submit" />
+                  </form>
+                  <br />
+                  <br />
+                  <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
+                      <thead>
+                      <tr>
+                          <th width="90%" align="center">Files</th>
+                          <th align="center">Action</th>
+                      </tr>
+                      </thead>
+                      <?php
+                      $query=$conn->query("select * from upload order by id desc");
+                      while($row=$query->fetch()){
+                          $name=$row['name'];
+                          ?>
+                          <tr>
+
+                              <td>
+                                  &nbsp;<?php echo $name ;?>
+                              </td>
+                              <td>
+                                  <button class="alert-success"><a href="download.php?filename=<?php echo $name;?>">Download</a></button>
+                              </td>
+                          </tr>
+                      <?php }?>
+                  </table>
+              </div>
+          </div>
+      </div>
   </div>
 
   <div class="rightcolumn">
