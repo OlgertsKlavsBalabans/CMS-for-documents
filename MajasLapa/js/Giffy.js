1781212a83c9578fbbcf695f542ba84a73927fd4
Vreodium Laurence (｡･ ω)ゞ☆ <49081011+VreodiumLaurence@users.noreply.github.com>
2019-05-14 18:39:30 +0300

    Javascript Gif base
    3 gifi + Javascrit un small minor bugs, kuru taggad laboju.

---
+++ MajasLapa/js/Giffy.js
@@ -0,0 +1,28 @@
+var gif1 = ["..\images\gif1.gif", "..\images\gif2.gif", "..\images\gif3.gif"]
+var integer = 0;
+
+
+function show_image() {
+    var img = document.createElement("img");
+    img.src = gif1[integer]
+    img.id = "gif"
+    alt = ""
+    img.style = "width:12.5em;height:13.12em;" 
+   
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+
+    //This adds something that listens for clicks
+    document.getElementById('gif').addEventListener('click', function(e) {
+        if (integer == 2) {
+            integer = 0;
+        } else {
+            integer = integer + 1;
+        }
+        //This sets the source again
+        document.getElementById('gif').src = gif1[integer]
+    });
+}
+
+show_image()
